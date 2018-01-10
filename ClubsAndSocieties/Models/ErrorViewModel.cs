c0da3e5090d8248d3643d8ae18a69c09dacabb5d
mark o shea <s00015630@mail.itsligo.ie>
2018-01-10 15:38:36 +0000

    Add project files.

---
+++ ClubsAndSocieties/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClubsAndSocieties.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
